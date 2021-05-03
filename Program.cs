d3b6c6914a9f52fc97aa09699eae57f1e8b5b20b
Linus Å <71346418+LynkanRex@users.noreply.github.com>
2021-05-03 11:20:13 +0200

    Initial add: Uses lock(){ ... }

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace GP20_Multithreading
 {
@@ -6,7 +7,41 @@ namespace GP20_Multithreading
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ulong a = 0, b = 0, ab = 0;
+            object _lock = "true";
+            
+            new Thread(() =>
+            {
+                while(true)
+                {
+                    lock (_lock)
+                    {
+                        a++;
+                        ab++;
+                    }
+                }
+            }).Start();
+            new Thread(() =>
+            {
+                while(true)
+                {
+                    lock (_lock)
+                    {
+                        b++;
+                        ab++;
+                    }
+                }
+            }).Start();
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    Console.WriteLine($"a: {a}, b: {b}, ab: {ab}");
+                    Thread.Sleep(100);
+                }
+            }).Start();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
         }
     }
 }
