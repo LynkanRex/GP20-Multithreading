5654483ef0eb05e9638c336541b586e0fce7ea1d
Linus Å <71346418+LynkanRex@users.noreply.github.com>
2021-05-03 11:18:37 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GP20_Multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
